5deed0573729b51b988e3096999945b281f8c172
Robin Palmer <robin.p6@gso.schule.koeln>
2022-11-14 12:48:00 +0100

    Use Pinia for state management of user login

---
+++ frontend/src/views/WelcomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+const user = useUserStore();
+</script>
+
+<template>
+  <main>
+    <header class="bg-white">
+      <div class="flex flex-col md:flex-row md:items-center gap-4 md:gap-12">
+        <img alt="Vue logo" class="logo h-20 max-w-max pl-4 md:pl-12" src="@/assets/cropped-LOGO-GSO_neu.png" />
+        <div class="flex flex-row items-center gap-4 p-4 md:gap-12 md:h-24">
+          <div><a href="#" @click.prevent="user.signIn()">Fake Login</a></div>
+          <div><RouterLink to="/login">Einloggen</RouterLink></div>
+          <div><RouterLink to="/login">Registrieren</RouterLink></div>
+          <div><RouterLink to="/login">Benutzerhilfe</RouterLink></div>
+        </div>
+      </div>
+      <div class="relative">
+        <video id="wp-custom-header-video" autoplay loop playsinline class="w-100" src="@/assets/20201106_GSO-Loop_Herbst-Impressionen_1920x900.mp4"></video>
+        <div class="absolute inset-0 flex items-center justify-center text-white text-center text-4xl md:text-5xl lg:text-6xl">Georg-Simon-Ohm-Berufskolleg</div>
+      </div>
+    </header>
+  </main>
+</template>
