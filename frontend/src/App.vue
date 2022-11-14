4feb942d5efd5fb33edb75c65364d5c4934f636c
Robin Palmer <robin.p6@gso.schule.koeln>
2022-11-14 12:25:29 +0100

    Add header to every page, change background color

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="bg-gso-blue text-3xl md:text-4xl lg:text-5xl p-4 text-white text-center">Mitfahrer-Datenbank</div>
   <RouterView />
 </template>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,8 +4,7 @@
 
 <template>
   <main>
-    <header>
-      <div class="bg-gso-blue text-3xl md:text-4xl lg:text-5xl p-4 text-white text-center">Mitfahrer-Datenbank</div>
+    <header class="bg-white">
       <div class="flex flex-col md:flex-row md:items-center gap-4 md:gap-12">
         <img alt="Vue logo" class="logo h-20 max-w-max pl-4 md:pl-12" src="@/assets/cropped-LOGO-GSO_neu.png" />
         <div class="flex flex-row items-center gap-4 p-4 md:gap-12 md:h-24">
