b6a5fa8c12f0c6f81c9364a15941dce0761e4229
Jakob Szczepanski <jakob.szczepanski@obi.de>
2022-11-14 11:53:04 +0100

    Add basic theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#5B9BD5',
+    },
+    fontFamily: {
+      'sans': ['Calibri', 'Helvetica', 'Candara', 'sans-serif']
+    }
   },
   plugins: [],
 }
