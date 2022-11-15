2c7c3671b0b74e0d8e8a84068154bf9426200ff0
Jakob Szczepanski <jakob.szczepanski@obi.de>
2022-11-15 11:07:49 +0100

    Create RegisterView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import LoginView from '@/views/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import RootView from '../views/RootView.vue'
 import RegisterDetailsView from '../views/RegisterDetailsView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,10 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: RootView
+    },
+    {
+      path: '/register',
+      component: RegisterView
     },
     {
       path: '/login',
