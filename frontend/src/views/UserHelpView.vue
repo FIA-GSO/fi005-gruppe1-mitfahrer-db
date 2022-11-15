b919b1c7c31db7ced95397ccf30f6876be4ba98a
Jakob Szczepanski <jakob.szczepanski@obi.de>
2022-11-15 12:13:53 +0100

    Add UserHelp.vue

--- frontend/src/views/UserHelpView.vue
+++ frontend/src/views/UserHelpView.vue
@@ -1,10 +1,4 @@
-<script setup lang="ts">
-import { useUserStore } from '@/stores/user'
-const user = useUserStore();
-</script>
-
 <template>
-  <main>
     <header class="bg-white">
       <div class="flex flex-col md:flex-row md:items-center gap-4 md:gap-12">
         <RouterLink to="/" class="pl-4 md:pl-12 mx-auto lg:mx-0 p-2">
@@ -16,10 +10,6 @@ const user = useUserStore();
           <div><RouterLink to="/help">Benutzerhilfe</RouterLink></div>
         </div>
       </div>
-      <div class="relative">
-        <video id="wp-custom-header-video" autoplay loop playsinline class="w-100" src="@/assets/20201106_GSO-Loop_Herbst-Impressionen_1920x900.mp4"></video>
-        <div class="absolute inset-0 flex items-center justify-center text-white text-center text-4xl md:text-5xl lg:text-6xl">Georg-Simon-Ohm-Berufskolleg</div>
-      </div>
     </header>
-  </main>
+    <h1>Benutzerhilfe</h1>
 </template>
